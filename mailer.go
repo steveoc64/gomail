44048d0e7970c54b79861cfb8f544759db56b264
steveoc <steveoc64@gmail.com>
2019-09-07 10:11:58 +1000

    initial version

---
+++ mailer.go
@@ -0,0 +1,78 @@
+package gomail
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/smtp"
+)
+
+type Mailer struct {
+	Server string
+	Port   int
+	User   string
+	Pass   string
+}
+
+func New(server string, user, pass string) *Mailer {
+	return &Mailer{
+		Server: server,
+		Port:   587,
+		User:   user,
+		Pass:   pass,
+	}
+}
+
+func (m *Mailer) Send(from, to, subject, msg string) error {
+	// Connect to the remote SMTP server.
+	c, err := smtp.Dial(fmt.Sprintf("%s:%d", m.Server, m.Port))
+	if err != nil {
+		return err
+	}
+
+	if err := c.Hello(m.Server); err != nil {
+		return err
+	}
+
+	config := &tls.Config{
+		ServerName:         m.Server,
+		InsecureSkipVerify: true,
+	}
+	if err := c.StartTLS(config); err != nil {
+		return err
+	}
+
+	if err := c.Auth(smtp.PlainAuth("", m.User, m.Pass, m.Server)); err != nil {
+		return err
+	}
+
+	// Set the sender and recipient first
+	if err := c.Mail(from); err != nil {
+		return err
+	}
+	if err := c.Rcpt(to); err != nil {
+		return err
+	}
+
+	// Send the email subject and body.
+	wc, err := c.Data()
+	if err != nil {
+		return err
+	}
+	if _, err := wc.Write([]byte(fmt.Sprintf("Subject: %s\n", subject))); err != nil {
+		return err
+	}
+	if _, err := wc.Write([]byte(msg)); err != nil {
+		return err
+	}
+	err = wc.Close()
+	if err != nil {
+		return err
+	}
+
+	// Send the QUIT command and close the connection.
+	err = c.Quit()
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ mailer_test.go
@@ -0,0 +1,34 @@
+package gomail
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type ConfigData struct {
+	Server   string
+	Username string
+	Password string
+	From     string
+	To       string
+	Subject  string
+	Msg      string
+}
+
+func TestSend(t *testing.T) {
+	// get the params to setup the server
+	config := &ConfigData{}
+	c, err := ioutil.ReadFile("config.json")
+	assert.Nil(t, err)
+	assert.Nil(t, json.Unmarshal(c, config))
+	t.Log(config.Server, config.Username, config.Password)
+
+	// setup a mailer
+	m := New(config.Server, config.Username, config.Password)
+	assert.Nil(t, m.Send(config.Username, config.To, config.Subject, config.Msg))
+
+	// Check that the mail ended up in the target mailbox
+}
